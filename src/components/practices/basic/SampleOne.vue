ac76d2269267765b9da007338fbc56ebcca2416e
mingyukang5420 <mingyukang@kakao.com>
2026-08-24 16:30:50 +0900

    feat: reactivity example

---
+++ src/components/practices/basic/SampleOne.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// 1. 일반 변수 (화면이 실시간으로 바뀌지 않음)
+let normalCount = 0
+
+// 2. 반응성 변수 (화면이 실시간으로 바뀜)
+const vueCount = ref(0)
+
+// 3. 콘솔 로그용 스크립트
+onMounted(() => {
+  // DOM 그리고 이후에 querySelector로 버튼 찾기 위해서 onMounted 내부에 작성
+  const allButtons = document.querySelectorAll('button')
+
+  // 버튼 순회하면서 eventListener 추가
+  allButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      console.log(event.target)
+    })
+  })
+})
+</script>
+
+<template>
+  <div class="practice-section">
+    <h2>Hello Skala-Vue</h2>
+    <h3>일반 변수 클릭: {{ normalCount }}</h3>
+    <button @click="normalCount++">일반 변수 증가</button>
+    <br />
+    <h3>Vue 반응성 변수 클릭: {{ vueCount }}</h3>
+    <button @click="vueCount++">Vue 변수 증가</button>
+  </div>
+</template>
