78b6638af7dfb927eef68d048522f41253f3cd5e
mingyukang5420 <mingyukang@kakao.com>
2026-08-24 16:53:27 +0900

    chore: v-html components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
+// 1. 학습환경 구성
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+
+// 2. Vue Directive
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
 </script>
 
 <template>
   <h1>Hello Skala-Vue</h1>
   <div style="padding: 20px">
+    <h2>1. 학습환경구성</h2>
     <SampleOne />
     <hr />
     <SampleTwo />
+    <h2>2. Vue Directive</h2>
+    <VueHtml />
+    <hr />
+    <VueHtmlXss />
   </div>
 </template>
