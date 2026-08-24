4f7764e8fc1a85fa5f19aa3dc803b5b9f92e97ec
mingyukang5420 <mingyukang@kakao.com>
2026-08-24 16:32:16 +0900

    chore: construct educational environment

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+</script>
+
+<template>
+  <h1>Hello Skala-Vue</h1>
+  <div style="padding: 20px">
+    <SampleOne />
+    <hr />
+    <SampleTwo />
+  </div>
+</template>
