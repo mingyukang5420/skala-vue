21ea5cda569f11885fd0253422cdbcda90cdcdd4
mingyukang5420 <mingyukang@kakao.com>
2026-08-25 16:49:37 +0900

    feat: add navigation bar component

---
+++ src/components/layout/AppNavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navigation-bar">
+    <RouterLink class="nav-item" :to="{ name: 'weather-home' }">🌦️ 날씨</RouterLink>
+    <span class="divider">|</span>
+    <RouterLink class="nav-item" :to="{ name: 'practice-day1' }">Day 1</RouterLink>
+    <span class="divider">|</span>
+    <RouterLink class="nav-item" :to="{ name: 'practice-day2' }">Day 2</RouterLink>
+    <span class="divider">|</span>
+    <RouterLink class="nav-item" :to="{ name: 'practice-day3' }">Day 3</RouterLink>
+    <span class="divider">|</span>
+    <RouterLink class="nav-item" :to="{ name: 'practice-day4' }">Day 4</RouterLink>
+  </nav>
+</template>
