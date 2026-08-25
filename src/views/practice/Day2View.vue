f2839ea6c44288ba8acc3848313f45f2f70beab1
mingyukang5420 <mingyukang@kakao.com>
2026-08-25 17:28:55 +0900

    chore: day2 watcher practice

--- src/views/practice/Day2View.vue
+++ src/views/practice/Day2View.vue
@@ -2,6 +2,16 @@
 // 1. Reactive State
 import ReactiveRef from '@/components/practices/composition/ReactiveRef.vue'
 import ReactivePractice from '@/components/practices/composition/ReactivePractice.vue'
+
+// 2. Computed & Watchers
+import ComputedBasic from '@/components/practices/composition/ComputedBasic.vue'
+import WatchersBasic from '@/components/practices/composition/WatchersBasic.vue'
+import WatchersMulti from '@/components/practices/composition/WatchersMulti.vue'
+import WatchersDeep from '@/components/practices/composition/WatchersDeep.vue'
+import WatchersReactive from '@/components/practices/composition/WatchersReactive.vue'
+import WatchersRefArray from '@/components/practices/composition/WatchersRefArray.vue'
+import WatchersReactiveArray from '@/components/practices/composition/WatchersReactiveArray.vue'
+import WatchersWatchEffect from '@/components/practices/composition/WatchersWatchEffect.vue'
 </script>
 
 <template>
@@ -9,7 +19,25 @@ import ReactivePractice from '@/components/practices/composition/ReactivePractic
     <h1>Day 2 - Composition API 실습</h1>
     <h2>1. Reactive State</h2>
     <ReactiveRef />
+    <hr />
     <ReactivePractice />
-
+    <hr />
+    <h2>2. Computed & Watchers</h2>
+    <ComputedBasic />
+    <hr />
+    <WatchersBasic />
+    <hr />
+    <WatchersMulti />
+    <hr />
+    <WatchersDeep />
+    <hr />
+    <WatchersReactive />
+    <hr />
+    <WatchersRefArray />
+    <hr />
+    <WatchersReactiveArray />
+    <hr />
+    <WatchersWatchEffect />
+    <hr />
   </div>
 </template>
