e3ff7c2b65dec9364c911554bc6bf467a1650103
mingyukang5420 <mingyukang@kakao.com>
2026-08-25 16:49:39 +0900

    feat: add day2 practice view

---
+++ src/views/practice/Day2View.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ComputedPractice from '@/components/practices/composition/ComputedPractice.vue'
+import ReactivePractice from '@/components/practices/composition/ReactivePractice.vue'
+</script>
+
+<template>
+  <div class="practice-container">
+    <h1>Day 2 - Composition API 실습</h1>
+    <h2>1. computed</h2>
+    <ComputedPractice />
+    <hr />
+    <h2>2. reactive</h2>
+    <ReactivePractice />
+  </div>
+</template>
