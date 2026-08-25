5ce09ae8f6bf372e6a052a4bae425642b02739aa
mingyukang5420 <mingyukang@kakao.com>
2026-08-25 11:29:19 +0900

    chore: adjust url to exercise multipage

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 멀티 페이지 설정 추가
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        exercise: fileURLToPath(new URL('./exercise.html', import.meta.url)),
+      },
     },
   },
 })
