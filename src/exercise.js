b4cf52882892ec582d9b2f9feb53d6c5e4462307
mingyukang5420 <mingyukang@kakao.com>
2026-08-25 15:09:11 +0900

    chore: add router and pinia to exercise app

--- src/exercise.js
+++ src/exercise.js
@@ -1,7 +1,14 @@
-// exercise.js
 import './assets/exercise.css'
 
 import { createApp } from 'vue'
-import AppExercise from './AppExercise.vue'
+import { createPinia } from 'pinia'
 
-createApp(AppExercise).mount('#app')
+import AppExercise from './AppExercise.vue'
+import router from './router'
+
+const appExercise = createApp(AppExercise)
+
+appExercise.use(createPinia())
+appExercise.use(router)
+
+appExercise.mount('#app')
