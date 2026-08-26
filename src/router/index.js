c56cea832c775bb66b13587d022d41f69f2b461c
mingyukang5420 <mingyukang@kakao.com>
2026-08-26 19:17:54 +0900

    refactor: split exercise routes by stage and add catch-all route

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WeatherHomeView from '../views/WeatherHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'weather-home',
-      component: WeatherHomeView,
+      redirect: { name: 'exercise-1' },
+    },
+    {
+      path: '/exercise',
+      redirect: { name: 'exercise-1' },
+    },
+    {
+      path: '/exercise/1',
+      name: 'exercise-1',
+      component: () => import('../views/exercise/Exercise1View.vue'),
+    },
+    {
+      path: '/exercise/2',
+      name: 'exercise-2',
+      component: () => import('../views/exercise/Exercise2View.vue'),
+    },
+    {
+      path: '/exercise/3',
+      name: 'exercise-3',
+      component: () => import('../views/exercise/Exercise3View.vue'),
+    },
+    {
+      path: '/exercise/4',
+      name: 'exercise-4',
+      component: () => import('../views/exercise/Exercise4View.vue'),
+      children: [
+        {
+          path: '',
+          name: 'exercise-4-home',
+          component: () => import('../views/WeatherRouterHomeView.vue'),
+        },
+        {
+          path: 'about',
+          name: 'exercise-4-about',
+          component: () => import('../views/WeatherAboutView.vue'),
+        },
+        {
+          path: 'weather/:cityId',
+          name: 'exercise-4-detail',
+          component: () => import('../views/WeatherDetailView.vue'),
+        },
+      ],
     },
     {
       path: '/practice',
@@ -32,6 +71,11 @@ const router = createRouter({
       path: '/practice/day4',
       name: 'practice-day4',
       component: () => import('../views/practice/Day4View.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFoundView.vue'),
     },
   ],
 })
