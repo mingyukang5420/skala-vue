d9e5532b84bbd593584c330aac3bfeab317a4ecb
mingyukang5420 <mingyukang@kakao.com>
2026-08-26 13:29:44 +0900

    chore: add day3 component practice view

--- src/views/practice/Day3View.vue
+++ src/views/practice/Day3View.vue
@@ -1,6 +1,34 @@
+<script setup>
+// 1. Component Lifecycle
+import LifecycleParent from '@/components/practices/component/LifecycleParent.vue'
+
+// 2. Props & Emits
+import PropsEmitsParent from '@/components/practices/component/PropsEmitsParent.vue'
+
+// 3. Component Slot
+import SlotDefaultParent from '@/components/practices/component/SlotDefaultParent.vue'
+import SlotNamedParent from '@/components/practices/component/SlotNamedParent.vue'
+import SlotScopedParent from '@/components/practices/component/SlotScopedParent.vue'
+</script>
+
 <template>
   <div class="practice-container">
-    <h1>Day 3</h1>
-    <p>준비중입니다. 추후 실습 내용이 추가될 예정입니다.</p>
+    <h1>Day 3 - Component 실습</h1>
+    <h2>1. Component Lifecycle</h2>
+    <hr />
+    <LifecycleParent />
+    <hr />
+    <h2>2. Props & Emits</h2>
+    <hr />
+    <PropsEmitsParent />
+    <hr />
+    <h2>3. Component Slot</h2>
+    <hr />
+    <SlotDefaultParent />
+    <hr />
+    <SlotNamedParent />
+    <hr />
+    <SlotScopedParent />
+    <hr />
   </div>
 </template>
