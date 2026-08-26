4f8b08046af4091780a70a6a03ff34f7fc38380d
mingyukang5420 <mingyukang@kakao.com>
2026-08-26 16:57:15 +0900

    chore: add weather component section to home view

--- src/views/WeatherHomeView.vue
+++ src/views/WeatherHomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import WeatherMockup from '@/components/exercise/WeatherMockup.vue'
 import WeatherComposition from '@/components/exercise/WeatherComposition.vue'
+import WeatherParent from '@/components/exercise/WeatherParent.vue'
 </script>
 
 <template>
@@ -13,5 +14,10 @@ import WeatherComposition from '@/components/exercise/WeatherComposition.vue'
     <h1>⛅ 과제 2: 날씨 (컴포지션)</h1>
     <hr />
     <WeatherComposition />
+  </div>
+  <div class="app-container">
+    <h1>⛅ 과제 3: 날씨 (컴포넌트)</h1>
+    <hr />
+    <WeatherParent />
   </div>
 </template>
