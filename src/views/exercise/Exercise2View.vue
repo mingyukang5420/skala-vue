dc9650bc6b141c0919e076fcc6f3eb27790a95d6
mingyukang5420 <mingyukang@kakao.com>
2026-08-26 19:17:54 +0900

    chore: add exercise2 view

---
+++ src/views/exercise/Exercise2View.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WeatherComposition from '@/components/exercise/WeatherComposition.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <h1>⛅ 과제 2: 날씨 (컴포지션)</h1>
+    <hr />
+    <WeatherComposition />
+  </div>
+</template>
