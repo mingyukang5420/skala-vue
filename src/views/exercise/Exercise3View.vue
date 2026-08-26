ad6414b3b11694acf3bb4c4f19918e28724b4728
mingyukang5420 <mingyukang@kakao.com>
2026-08-26 19:17:54 +0900

    chore: add exercise3 view

---
+++ src/views/exercise/Exercise3View.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WeatherParent from '@/components/exercise/WeatherParent.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <h1>⛅ 과제 3: 날씨 (컴포넌트)</h1>
+    <hr />
+    <WeatherParent />
+  </div>
+</template>
