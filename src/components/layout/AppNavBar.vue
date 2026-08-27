6d5b6ac13948045c442ab74a2c77f0c412c6ae48
mingyukang5420 <mingyukang@kakao.com>
2026-08-27 13:43:01 +0900

    chore: default exercise4 to 6 nav link to dashboard tab

--- src/components/layout/AppNavBar.vue
+++ src/components/layout/AppNavBar.vue
@@ -10,11 +10,10 @@ import { RouterLink } from 'vue-router'
     <span class="divider">|</span>
     <RouterLink class="nav-item" :to="{ name: 'exercise-3' }">과제3</RouterLink>
     <span class="divider">|</span>
-    <RouterLink class="nav-item" :to="{ name: 'exercise-4' }">과제4</RouterLink>
-    <span class="divider">|</span>
-    <RouterLink class="nav-item" :to="{ name: 'exercise-5' }">과제5</RouterLink>
-    <span class="divider">|</span>
-    <RouterLink class="nav-item" :to="{ name: 'exercise-6' }">과제6</RouterLink>
+    <!-- exercise-6는 children(home/about/detail)을 가진 부모 라우트라, 부모 이름으로 이동하면
+         Vue Router가 기본 자식(exercise-6-home)을 matched에 안 넣어서 대시보드 탭이 active로 안 잡힘.
+         진입하자마자 대시보드가 active로 보이는 게 UX상 맞다고 판단해서 자식 라우트 이름으로 직접 연결함. -->
+    <RouterLink class="nav-item" :to="{ name: 'exercise-6-home' }">과제4-6</RouterLink>
     <span class="divider">|</span>
     <RouterLink class="nav-item" :to="{ name: 'practice-day1' }">Day 1</RouterLink>
     <span class="divider">|</span>
